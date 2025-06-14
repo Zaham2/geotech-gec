febe7685b29038f1755a63c478ac09d573126f26
Zaham2 <zaham2@yahoo.com>
2025-06-14 07:23:06 +0300

    Backend-dev docker configs

---
+++ backend/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+echo "🕓 Waiting for PostgreSQL to be ready at $POSTGRES_HOST:$POSTGRES_PORT..."
+
+until nc -z -v -w30 "$POSTGRES_HOST" "$POSTGRES_PORT"; do
+  echo "Waiting for database connection..."
+  sleep 2
+done
+
+echo "✅ PostgreSQL is up - running Prisma migrations..."
+npx prisma migrate deploy
+
+echo "🚀 Starting NestJS application..."
+exec npm run start:dev:watch
